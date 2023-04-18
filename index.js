e182404c98da28f706f62d583e72bf493d61ddc5
Elijah Nti <elijahnti175@gmail.com>
2023-04-18 01:37:34 +0100

    upgrade 1

---
+++ index.js
@@ -0,0 +1,7 @@
+let body = document.querySelector('body');
+//for dark theme
+    let dark = document.querySelector('#Dark').addEventListener('click', function(){
+        body.style.backgroundColor = 'black';
+        body.style.color = '#fff';
+    });
+    
